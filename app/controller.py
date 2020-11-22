c0d75bf2a024c01ae25425ba30f0db2d19ae267b
Alan Hogarth <alan.hogarth@outlook.com>
2020-11-22 17:27:26 +0000

    revisions to template and route

--- app/controller.py
+++ app/controller.py
@@ -2,6 +2,7 @@ from flask import render_template, request, redirect
 from app import app
 from app.models.player import *
 from app.models.game import *
+from random import choice
 
 @app.route('/<hand1>/<hand2>')
 def play_game(hand1, hand2):
@@ -9,10 +10,13 @@ def play_game(hand1, hand2):
     player_2 = Player("Player 2", hand2)
     current_game = Game(player_1, player_2)
     winner = current_game.get_winner()
-    return render_template("results.html", winner=winner)
+    
+    if winner != None:
+        result = winner.name
+    else:
+        result = None
 
-    # get winner variable into a form
-    #  winner = winner as render_template parameter
+    return render_template("results.html", player_1 = player_1.name, player_2 = player_2.name, hand1 = hand1, hand2 = hand2, result = result)
 
 
 @app.route('/welcome')
@@ -30,6 +34,15 @@ def game_play():
     add_move = request.form["hand"]
     return "logged"
    
+# @app.route('/computer')
+# def computer_play():
+#     moves = ["rock", "paper", "scissors"]
+#     random_move = choice(moves)
+    
+#     player_1 = Player("Player 1", "rock")
+#     player_2 = Player("Computer", random_move)
+#     current_game = Game(player_1, player_2)
+#     winner = current_game.get_winner()
     
     
     
--- app/models/game.py
+++ app/models/game.py
@@ -1,38 +1,26 @@
 class Game:
 
-    def __init__(self, player_1, player_2, computer):
+    def __init__(self, player_1, player_2):
         self.player_1 = player_1
         self.player_2 = player_2
-        self.computer = computer
+        
 
 
     def get_winner(self):
         if self.player_1.hand == "rock" and self.player_2.hand == "scissors":
-           return "Rock beats Scissors: player 1 wins"
+           return self.player_1
         elif self.player_1.hand == "scissors" and self.player_2.hand == "rock":
-            return "Rock beats Scissors: player 2 wins!"
-        elif self.player_1.hand == "rock" and self.player_2.hand == "rock":
-            return "Rock/Rock: It's a draw"
-        elif self.player_1.hand == "scissors" and self.player_2.hand == "scissors":
-            return "Scissors/Scissors: it's a draw"
+            return self.player_2
+        
         elif self.player_1.hand == "rock" and self.player_2.hand == "paper":
-            return "Paper beats Rock: player 2 wins!"
+            return self.player_2
         elif self.player_1.hand == "paper" and self.player_2.hand == "rock":
-            return "Paper beats Rock: player 1 wins!"
-        elif self.player_1.hand == "paper" and self.player_2.hand == "paper":
-            return "Paper/Paper: it's a draw"
+            return self.player_1
+        
         elif self.player_1.hand == "scissors" and self.player_2.hand == "paper":
-            return "Scissors beats Paper: player 1 wins!"
+            return self.player_1
         elif self.player_1.hand == "paper" and self.player_2.hand == "scissors":
-            return "Scissors beats Paper: player 2 wins!"
+            return self.player_2
         
-    import random
-
-    moves = ["rock", "paper", "scissors"]
-
-    def computer(self):
-        return random.choice(moves)
-        
-  
-
-
+        elif self.player_1.hand == self.player_2.hand:
+            return None
